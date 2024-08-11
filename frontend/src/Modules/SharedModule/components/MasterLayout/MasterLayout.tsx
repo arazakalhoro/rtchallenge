0616576f21460411c55890679ae119a9c4c2977f
Ali Raza <arazakalhoro@gmail.com>
2024-08-11 20:23:11 +0500

    removed unnecessary resources

--- frontend/src/Modules/SharedModule/components/MasterLayout/MasterLayout.tsx
+++ frontend/src/Modules/SharedModule/components/MasterLayout/MasterLayout.tsx
@@ -1,13 +1,11 @@
 import React from 'react'
 import Styles from "./MasterLayout.module.css"
-import Navbar from '../Navbar/Navbar'
 import SideBar from '../SideBar/SideBar'
 import { Outlet } from 'react-router-dom'
 
 export default function MasterLayout() {
   return (
 <>
-      <Navbar/>
  <div className={`${Styles.masterMain} d-flex`}>
         <div>
           <SideBar/>
